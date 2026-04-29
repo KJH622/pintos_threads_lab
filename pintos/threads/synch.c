a36d9f2861822776bfe63ed98a7f4ee5e0219790
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-04-29 11:09:46 +0900

    fix(synch): sema_up에서 waiters 우선순위 재정렬

--- pintos/threads/synch.c
+++ pintos/threads/synch.c
@@ -133,16 +133,18 @@ sema_up (struct semaphore *sema) {
 	ASSERT (sema != NULL);
 
 	old_level = intr_disable ();
-	if (!list_empty (&sema->waiters))
+	if (!list_empty (&sema->waiters)){
+      list_sort(&sema->waiters, pri_more, NULL);
+
 		thread_unblock (list_entry (list_pop_front (&sema->waiters),
 					struct thread, elem));
+   }
 
 	sema->value++;
 	intr_set_level (old_level);
 
-	if (!intr_context ()) {
-      thread_yield_if_needed ();
-   }
+   thread_yield_if_needed ();
+   
 }
 
 static void sema_test_helper (void *sema_);
